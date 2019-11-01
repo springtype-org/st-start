f43992065bc07ef907a6585f3b362f4ae797055a
kyr0 <info@aron-homberg.de>
2019-11-01 22:01:33 +0100

    chore: Caching strategy for peer dependency installation

--- src/function/execute.ts
+++ src/function/execute.ts
@@ -4,6 +4,7 @@ import { spawn } from "cross-spawn";
 export function execute(cmd: string, args: Array<string> = [], options: any = {}) {
   console.log(chalk.green(">"), chalk.white(`${cmd} ${args.join(" ")}`));
 
+  // @ts-ignore
   const result = spawn.sync(cmd, args, options);
   if (!options.stdio || (options.stdio !== "inherit" && options.stdio !== "ignore")) {
     return result.stdout.toString("utf8");
--- src/function/install-peer-dependencies.ts
+++ src/function/install-peer-dependencies.ts
@@ -1,16 +1,30 @@
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
 import { IBuildConfig } from './../interface/ibuild-config';
+import { getContextPath } from './config-getters';
 import { execute } from './execute';
 import { getPeerDependencies } from './get-peer-dependencies';
 
 export const installPeerDependencies = (config: IBuildConfig) => {
+    const peerDependencies = getPeerDependencies(config);
+    const missingPeerDependencies = [];
+    const devDependencies = JSON.parse(readFileSync(resolve(getContextPath(config), 'package.json'), 'utf8'))
+        .devDependencies;
 
-    const peerDependenciesToInstall = getPeerDependencies(config);
+    for (let dependency of peerDependencies) {
+        if (!devDependencies[dependency.split('@^')[0]]) {
+            missingPeerDependencies.push(dependency);
+        }
+    }
 
-    // TODO: Cache
+    const yarnVersion = execute('npm', ['info', 'yarn', 'version']).trim();
 
-    // make sure yarn is installed locally
-    execute('npm', ['i', 'yarn']);
+    if (yarnVersion.indexOf('npm') === 0) {
+        execute('npm', ['install', '-g', 'yarn']);
+    }
 
-    // install all required peer dependencies (filtered by features enabled)
-    execute('yarn', ['add', ...peerDependenciesToInstall, '--dev']);
+    if (missingPeerDependencies.length) {
+        // install all required peer dependencies (filtered by features enabled)
+        execute('yarn', ['add', ...missingPeerDependencies, '--dev', '--cwd', resolve(getContextPath(config))]);
+    }
 };
